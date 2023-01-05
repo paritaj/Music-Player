fd1a91a1436ed26d1a2ec918c1f6ba1fcdaf9fd1
parit <pvppcoe.ac.in>
2023-01-05 22:11:13 +0530

    final commit

---
+++ js/music-list.js
@@ -0,0 +1,29 @@
+let allMusic = [
+
+    {
+        name: "Intentions",
+        artist: "Justin Bieber",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Calm down",
+        artist: "Selena Gomez",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Vibez",
+        artist: "Zayn Malik",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Thank u,next",
+        artist: "Ariana Grande",
+        img: "music-4",
+        src: "music-4"
+    },
+
+
+];
